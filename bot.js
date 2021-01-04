61a5f0406be6f73545654dc67f81cfd1f6107389
Tomasz <tomaszber93@gmail.com>
2021-01-04 23:01:48 +0100

    new commands

--- bot.js
+++ bot.js
@@ -25,7 +25,14 @@ function gotMessage(msg) {
 		const index = Math.floor(Math.random() * replies.length);
 		msg.channel.send(replies[index]);
 	}
+	else if (msg.content === '!wilu') {
+		msg.channel.send('A któż to przyszedł? Pan maruda? Niszczyciel dobrej zabawy. Pogromca uśmiechów dzieci.');
+	}
+	else if (msg.content === '!laylaban') {
+		msg.channel.send('Layla dostaje bana za gołąbki!');
+	}
+	else if (msg.content === '!nagimiban') {
+		msg.channel.send('Nagimi has been permanently banned from this channel.');
+	}
 	console.log(msg.content);
 }
-
-console.log("test");
