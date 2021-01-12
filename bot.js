9556e0df4a7893441e0482ed0e4972e83e6edb8c
Tomasz <tomaszber93@gmail.com>
2021-01-12 01:33:24 +0100

    new commands

--- bot.js
+++ bot.js
@@ -1,18 +1,14 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const readline = require('readline');
 
 require('dotenv').config();
 
 client.login(process.env.BOTTOKEN);
 
-client.on('ready', redyDiscord);
-
-function redyDiscord() {
-	console.log('authenticated');
-}
-
 client.on('message', gotMessage);
+client.on('ready', onReady);
 
 const karpReplies = [
 	'Karp to najlepsza ryba!',
@@ -35,40 +31,54 @@ const facepalmGifs = [
 	'https://tenor.com/EOpu.gif'
 ]
 
+var commands = new Array();
+var commandMessages = new Array();
 var pastaCooldown = new Date();
 
+function onReady() {
+	console.log('authenticated');
+	const readInterface = readline.createInterface({
+		input: fs.createReadStream('messages.txt'),
+		output: process.stdout,
+		console: false
+	});
+	
+	readInterface.on('line', function(line) {
+		const singleCommandName = /^!\w*$/;
+		
+		if (singleCommandName.test(line)) {
+			commands.push(line);
+		}
+		else {
+			const messageLine = line.split('=');
+			commands.push(messageLine[0]);
+			commandMessages.push({
+				"command": messageLine[0],
+				"message": messageLine[1]
+			});
+		}
+	});
+	
+	readInterface.on('close', function(line) {
+		console.log(commands);
+		console.log('/////////////////');
+		console.log(commandMessages);
+	});
+}
+
 function gotMessage(msg) {
+	commandMessages.forEach(commandMessage => {
+		if (msg.content === commandMessage.command) {
+			msg.channel.send(commandMessage.message);
+		}
+	});
 	if (msg.content === '!karp') {
 		const index = Math.floor(Math.random() * karpReplies.length);
 		msg.channel.send(karpReplies[index]);
 	}
-	else if (msg.content === '!wilu') {
-		msg.channel.send('A któż to przyszedł? Pan maruda? Niszczyciel dobrej zabawy. Pogromca uśmiechów dzieci.');
-	}
-	else if (msg.content === '!laylaban') {
-		msg.channel.send('Layla dostaje bana za gołąbki!');
-	}
-	else if (msg.content === '!nagimiban') {
-		msg.channel.send('Nagimi has been permanently banned from this channel.');
-	}
-	else if (msg.content === '!myszon') {
-		msg.channel.send('Chowajcie swoje ciasteczka, nadchodzi on! Pożeracz wszystkiego słodkiego i mięsnego.');
-	}
-	else if (msg.content === '!bbo') {
-		msg.channel.send('Szykuj wodę! Nadszedł naczelny nawadniacz dzikiej rodziny.');
-	}
-	else if (msg.content === '!rafal') {
-		msg.channel.send('Team Rafał!');
-	}
 	else if (msg.content === '!gocha') {
 		const index = Math.floor(Math.random() * gochaReplies.length);
 		msg.channel.send(gochaReplies[index]);
-	}
-	else if (msg.content === '!maslo') {
-		msg.channel.send('Team masło!');
-	}
-	else if (msg.content === '!aris') {
-		msg.channel.send('Czy ktoś tu ma niedobór przytulania? Aris tu jest żeby wyściskać wszystkie swoje dzieci!');
 	}
 	else if (msg.content === '!pastakarp') {
 		var pastaTime = new Date();
@@ -86,19 +96,18 @@ function gotMessage(msg) {
 	}
 	else if (msg.content === '!facepalm') {
 		const index = Math.floor(Math.random() * (facepalmGifs.length + 2));
-		console.log(index);
 		if (index == facepalmGifs.length) {
-			console.log(index);
 			msg.channel.send({files: ['./images/bear.png']});
 		}
 		else if (index == facepalmGifs.length + 1) {
-			console.log(index);
 			msg.channel.send({files: ['./images/triple-face-palm.png']});
 		}
 		else {
-			console.log(index);
 			msg.channel.send(facepalmGifs[index]);
 		}
+	}
+	else if (msg.content === '!commands') {
+		msg.channel.send(commands.join(', '));
 	}
 	console.log(msg.content);
 }
